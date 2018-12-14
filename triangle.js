03ccaa200ba0fa540ca4b6d312e2f8797098fa0a
UmurerwaDivine <divinekagame@gmail.com>
2018-12-14 15:14:59 +0200

    added a paragraph in html file

--- triangle.js
+++ triangle.js
@@ -2,15 +2,18 @@ function myTriangle(){
     var sideOne=document.getElementById("nber").value;
     var sideTwo=document.getElementById("nber1").value;
     var sideThree=document.getElementById("nber2").value;
-    
-   if(sideOne===sideTwo && sideOne===sideThree){
+
+    if(sideOne === "" || sideTwo === "" || sideThree === "") {
+        alert("Please enter three values");
+    }
+  else if(sideOne===sideTwo && sideOne===sideThree){
        alert("This triangle is Equilateral");
    }
    else if(sideOne===sideTwo || sideOne===sideThree || sideTwo===sideThree ){
        alert("This triangle is isoscele");
    }
    else if(sideOne+sideTwo>sideThree || sideOne+sideThree>sideTwo || sideTwo+sideThree>sideOne){
-       alert("This triangle is Isoscele")
+       alert("This triangle is Iscalene")
    }
    else{
        ("this not a triangle");
